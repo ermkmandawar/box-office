4c86c6b8ba8b10dacf4c29a16779d9ee9b4a8a84
ermkmandawar <ermkmandawar7@gmail.com>
2022-08-11 11:30:04 +0530

    Routing Addes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={'HomePage'} />
+      <Route exact path="/starred" element={'Starred page'} />
+      <Route path="*" element={'Page not exist'} />
+    </Routes>
   );
 }
 
