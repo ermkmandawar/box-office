05837663f6fc52afd1b0f2cd0bf385c2cc96ad97
ermkmandawar <ermkmandawar7@gmail.com>
2022-08-12 10:02:55 +0530

    add search option and fetch api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../component/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>Home Page Content</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onChangedInput = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(results => console.log(results));
+  };
+
+  const onKeyPress = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type={'text'}
+        onChange={onChangedInput}
+        value={input}
+        onKeyDown={onKeyPress}
+      />
+      <button type={'button'} onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
